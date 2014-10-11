506e1200247e96796dc75ad4b7ca006d0e64ceca
danielmiola <daniel.miola@hotmail.com>
2014-10-11 17:08:55 -0300

    novas alteracoes na interface

--- src/java/control/ServletLote.java
+++ src/java/control/ServletLote.java
@@ -6,7 +6,6 @@
 package control;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.List;
 import javax.servlet.RequestDispatcher;
@@ -22,6 +21,13 @@ import persistence.*;
  * @author daniel
  */
 public class ServletLote extends HttpServlet {
+    
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
+    }
 
     public void busca(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, SQLException, DAOException{
@@ -48,35 +54,6 @@ public class ServletLote extends HttpServlet {
         RequestDispatcher rd = null;
 	rd = request.getRequestDispatcher("/viewLotes.jsp");
 	rd.forward(request, response);
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
     }
 
     /**
